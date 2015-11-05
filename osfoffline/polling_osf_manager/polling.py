f08754b8115c1b317c8c9930955ce49e703a3942
Matt Frazier <maf7sm@virginia.edu>
2015-11-04 20:48:44 -0500

    Fix tuple building method

--- osfoffline/polling_osf_manager/polling.py
+++ osfoffline/polling_osf_manager/polling.py
@@ -110,48 +110,23 @@ class Poll(object):
         assert None not in local_list
         assert None not in remote_list
 
-        combined_list = local_list + remote_list
-        sorted_combined_list = sorted(combined_list, key=self.get_id)
+        local_files = {}
+        remote_files = {}
 
-        local_remote_tuple_list = []
-        i = 0
-        while i < len(sorted_combined_list):
+        for local in local_list:
+            assert isinstance(local, Base)
+            local_files[local.id] = local
 
-            both_exist = i + 1 < len(sorted_combined_list) and \
-                         self.get_id(sorted_combined_list[i]) == \
-                         self.get_id(sorted_combined_list[i + 1])
-            if both_exist:
+        for remote in remote_list:
+            assert isinstance(remote, RemoteObject)
+            remote_files[remote.id] = remote
 
-                # (local, remote)
-                if isinstance(sorted_combined_list[i], RemoteObject):  # remote
-                    new_tuple = (sorted_combined_list[i + 1], sorted_combined_list[i])
-                elif isinstance(sorted_combined_list[i], Base):  # local
-                    new_tuple = (sorted_combined_list[i], sorted_combined_list[i + 1])
-                else:
-                    raise TypeError('invalid type: {}'.format(type(sorted_combined_list[i])))
+        return [
+            (local_files.get(fid), remote_files.get(fid))
+            for fid in set(list(local_files.keys()) + list(remote_files.keys()))
+        ]
 
-                # add an extra 1 because both values should be added to tuple list
-                i += 1
-            elif isinstance(sorted_combined_list[i], RemoteObject):
-
-                new_tuple = (None, sorted_combined_list[i])
-
-            else:
-
-                new_tuple = (sorted_combined_list[i], None)
-
-            local_remote_tuple_list.append(new_tuple)
-            i += 1
-
-        for local, remote in local_remote_tuple_list:
-            assert isinstance(local, Base) or local is None
-            assert isinstance(remote, RemoteObject) or remote is None
-            if isinstance(local, Base) and isinstance(remote, dict):
-                assert local.osf_id == remote.id
-
-        return local_remote_tuple_list
-
-    # Check
+    
 
     @asyncio.coroutine
     def check_osf(self, remote_user):
