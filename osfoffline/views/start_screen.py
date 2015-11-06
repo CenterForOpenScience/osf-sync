f02e22b94758acd6a702fbe6992f563868c9cf4a
Matt Frazier <maf7sm@virginia.edu>
2015-11-05 23:20:15 -0500

    Alphabetize imports

--- osfoffline/views/start_screen.py
+++ osfoffline/views/start_screen.py
@@ -1,23 +1,23 @@
 import asyncio
-import logging
 import concurrent
 from flask import request
+import logging
 
-import furl
 import aiohttp
+import furl
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import QDialog, QMessageBox
 from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
 
 from osfoffline import settings
-from osfoffline.utils.debug import debug_trace
 from osfoffline.database_manager.db import session
 from osfoffline.database_manager.utils import save
 from osfoffline.database_manager.models import User
-from osfoffline.views.rsc.startscreen import Ui_startscreen
 from osfoffline.exceptions.osf_exceptions import OSFAuthError
 from osfoffline.polling_osf_manager.osf_query import OSFQuery
 from osfoffline.polling_osf_manager.remote_objects import RemoteUser
+from osfoffline.utils.debug import debug_trace
+from osfoffline.views.rsc.startscreen import Ui_startscreen
 
 
 class StartScreen(QDialog):
