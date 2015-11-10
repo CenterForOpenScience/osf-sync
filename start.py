b23cd0e0cb5e11e4d74313f86f3086102bc24813
Nan Chen <nc6r@virginia.edu>
2015-11-10 10:58:16 -0500

    single instance

--- start.py
+++ start.py
@@ -1,13 +1,24 @@
+import os
 import sys
 
 from PyQt5.QtWidgets import (QApplication, QMessageBox, QSystemTrayIcon)
 from osfoffline.application.main import OSFApp
 from osfoffline import utils
-
+from server import SingleInstance
 
 def start():
     # Start logging all events
     utils.start_app_logging()
+    if os.name == 'nt':
+        single_app = SingleInstance()
+
+        if single_app.aleradyrunning():
+            QMessageBox.critical(
+                None,
+                "Systray",
+                "OSF-Offline is already running. Check out the system tray."
+            )
+            exit(0)
 
     app = QApplication(sys.argv)
 
