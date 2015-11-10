74af7a62a9ebb70d7d208f9ce0418f54f81ebaef
Nan Chen <nc6r@virginia.edu>
2015-11-10 11:40:05 -0500

    add check for os

--- start.py
+++ start.py
@@ -1,21 +1,40 @@
 import os
 import sys
+import __main__
 
 from PyQt5.QtWidgets import (QApplication, QMessageBox, QSystemTrayIcon)
 from osfoffline.application.main import OSFApp
 from osfoffline import utils
 
 
+def isOnlyInstance():
+    # Determine if there are more than the current instance of the application
+    # running at the current time.
+    return os.system("(( $(ps -ef | grep python | grep '[" +
+                     __main__.__file__[0] + "]" + __main__.__file__[1:] +
+                     "' | wc -l) > 1 ))") != 0
+
+
 def start():
     # Start logging all events
     utils.start_app_logging()
-    if os.name == 'nt':
+    if sys.platform == 'win32':
         from server import SingleInstance
         single_app = SingleInstance()
 
         if single_app.aleradyrunning():
             warn_app = QApplication(sys.argv)
-            QMessageBox.critical(
+            QMessageBox.information(
+                None,
+                "Systray",
+                "OSF-Offline is already running. Check out the system tray."
+            )
+            warn_app.quit()
+            exit(0)
+    elif sys.platform == 'darwin':
+        if not isOnlyInstance():
+            warn_app = QApplication(sys.argv)
+            QMessageBox.information(
                 None,
                 "Systray",
                 "OSF-Offline is already running. Check out the system tray."
