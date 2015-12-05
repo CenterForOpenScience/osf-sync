9c264403dfb7d55abeba9d1268588e47204389c2
Chris Seto <chriskseto@gmail.com>
2015-12-05 17:55:15 -0500

    remove unused code

--- osfoffline/database/utils.py
+++ osfoffline/database/utils.py
@@ -1,9 +1,5 @@
 import shutil
-import logging
 
-from contextlib import contextmanager
-
-from osfoffline.database import session
 from osfoffline import settings
 
 
@@ -12,23 +8,9 @@ def save(session, *items_to_save):
         session.add(item)
     try:
         session.commit()
-    except:
-        logging.exception('Error saving to the database')
+    except Exception:
         session.rollback()
         raise
-
-
-# @contextmanager
-# def session_scope():
-#     """Provide a transactional scope around a series of operations."""
-#     try:
-#         yield session
-#         session.commit()
-#     except:
-#         session.rollback()
-#         raise
-#     finally:
-#         session.close()
 
 
 def remove_db():
