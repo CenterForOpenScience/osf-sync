606eb073162958e74d73937659455ea1ffc447c9
Chris Seto <chriskseto@gmail.com>
2015-12-11 05:59:58 -0500

    Swallow notifications

--- sync.py
+++ sync.py
@@ -2,4 +2,6 @@
 from osfoffline.application.background import BackgroundWorker
 
 background_worker = BackgroundWorker()
+# self.background_worker.set_intervention_cb(
+background_worker.set_notification_cb(lambda *_, **__: None)
 background_worker.run()
