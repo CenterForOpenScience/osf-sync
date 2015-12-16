0ea45f61b81d82551b7a4e81ae544a512e417a7e
Andy Boughton <abought@gmail.com>
2015-12-16 16:30:39 -0500

    Basic implementation of consolidated notifications - When a notification fires, wait several seconds and gather all notifications together in a burst. Ensure that the user never has to wait too long for feedback on progress by using a straight timer, instead of something more complicated (like a debounce)    - INFO messages can be grouped, but messages of a higher severity will always be displayed individually. - Settings cleanup    - Remove unused settings    - Tweak format of log messages    - Change ALERT_TIME to ALERT_DURATION, and unit to seconds (more consistent with python APIs)

--- osfoffline/settings/defaults.py
+++ osfoffline/settings/defaults.py
@@ -20,20 +20,18 @@ FILE_BASE = 'https://test-files.osf.io'
 # Interval (in seconds) to poll the OSF for server-side file changes
 REMOTE_CHECK_INTERVAL = 60 * 5  # Every 5 minutes
 
-# Time to keep alert messages on screen (in milliseconds); may not be configurable on all platforms
-ALERT_TIME = 3000  # ms
+# Time to keep alert messages on screen (in seconds); may not be configurable on all platforms
+ALERT_DURATION = 5.0  # sec
 
 LOG_LEVEL = 'INFO'
 
 # Logging configuration
-FILE_FORMATTER = '[%(asctime)s][%(threadName)s][%(filename)s][%(levelname)s][%(name)s]: %(message)s'
+FILE_FORMATTER = '[%(levelname)s][%(asctime)s][%(threadName)s][%(name)s]: %(message)s'
 
 IGNORED_NAMES = ['.DS_Store', 'lost+found', 'Desktop.ini']
 IGNORED_PATTERNS = ['*.DS_Store', '*lost+found', '*Desktop.ini']
 
 OSF_STORAGE_FOLDER = 'OSF Storage'
-
-LOCAL_DELETE_THRESHOLD = 10
 
 # wab~,vmc,vhd,vo1,vo2,vsv,vud,vmdk,vmsn,vmsd,hdd,vdi,vmwarevm,nvram,vmx,vmem,iso,dmg,sparseimage,wim,ost,o,qtch,log
 # wab~,vmc,vhd,vdi,vo1,vo2,vsv,vud,iso,dmg,sparseimage,sys,cab,exe,msi,dll,dl_,wim,ost,o,qtch,log,ithmb,vmdk,vmem,vmsd,vmsn,vmss,vmx,vmxf,menudata,appicon,appinfo,pva,pvs,pvi,pvm,fdd,hds,drk,mem,nvram,hdd
