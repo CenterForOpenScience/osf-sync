f980835195f2b3938050d3ad83c418100cc2c5e9
Andy Boughton <abought@gmail.com>
2015-12-17 11:49:45 -0500

    Setup and helpers for flake8

--- tasks.py
+++ tasks.py
@@ -17,3 +17,8 @@ def qt_gen():
     run('pyuic5 ./osfoffline/gui/qt/static/login.ui -o ./osfoffline/gui/qt/generated/login.py')
     run('pyuic5 ./osfoffline/gui/qt/static/preferences.ui -o ./osfoffline/gui/qt/generated/preferences.py')
     run('pyrcc5 ./osfoffline/gui/qt/static/resources.qrc -o ./osfoffline/gui/qt/generated/resources.py')
+
+
+@task(aliases=['flake8'])
+def flake():
+    run('flake8 osfoffline/', echo=True)
