51d80f8361bcfa586fb55c22476937480681cb3e
Andy Boughton <abought@gmail.com>
2015-12-17 15:14:56 -0500

    No need to define command twice
    Make options consistent with OSF repo command, and only include application directly. (exclude build folders etc)

--- tasks.py
+++ tasks.py
@@ -1,9 +1,9 @@
 from invoke import task, run
 
 
-@task
+@task(aliases=['flake8'])
 def flake():
-    run('flake8 . --config=./setup.cfg', pty=True)
+    run('flake8 osfoffline/', echo=True)
 
 
 @task
@@ -17,8 +17,3 @@ def qt_gen():
     run('pyuic5 ./osfoffline/gui/qt/static/login.ui -o ./osfoffline/gui/qt/generated/login.py')
     run('pyuic5 ./osfoffline/gui/qt/static/preferences.ui -o ./osfoffline/gui/qt/generated/preferences.py')
     run('pyrcc5 ./osfoffline/gui/qt/static/resources.qrc -o ./osfoffline/gui/qt/generated/resources.py')
-
-
-@task(aliases=['flake8'])
-def flake():
-    run('flake8 osfoffline/', echo=True)
