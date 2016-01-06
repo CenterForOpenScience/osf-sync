7b0ffb21a72fcc36245cbb2352a87d135ef7f414
Andy Boughton <abought@gmail.com>
2016-01-06 16:59:08 -0500

    Deletion of directory should override/consolidate any other events lower down.
    CAUTION: If watchdog ever fires a Delete + Create event, this might override the child events with the directory delete, and cause all the child file create events to be lost. This would be... tragic.

--- osfoffline/sync/ext/watchdog.py
+++ osfoffline/sync/ext/watchdog.py
@@ -25,9 +25,6 @@ class ConsolidatedEventHandler(PatternMatchingEventHandler):
 
     def dispatch(self, event):
         with self.lock:
-            if event.is_directory and event.event_type == 'modified':
-                return
-
             src_parts = event.src_path.split(os.path.sep)
             if not hasattr(event, 'dest_path'):
                 dest_parts = repeat(None)
@@ -36,11 +33,22 @@ class ConsolidatedEventHandler(PatternMatchingEventHandler):
 
             parts = list(zip(src_parts, dest_parts))
 
+            # Windows represents folder deletes incorrectly as file deletes, and as
+            # result we can't trust event.is_directory to check whether or not delete
+            # events need to be consolidated
+            consolidate = event.is_directory
+            if event.event_type == 'deleted':
+                consolidate = (parts in self._event_cache)
+
+            if event.is_directory and event.event_type == 'modified':
+                return
+
             try:
                 if event.is_directory and event.event_type == 'created':
                     self._create_cache.extend(self._event_cache.children(parts))
 
-                if not event.is_directory and parts in self._event_cache and self._event_cache[parts].event_type == 'deleted':
+                if not consolidate and parts in self._event_cache and self._event_cache[parts].event_type == 'deleted':
+                    # TODO: Deleting a folder with files still causes error
                     # Would technically be more correct to create a modified event but only event_type is checked, not the type
                     # Turn deletes followed by creates into updates IE saving in vim or replacing a file in finder
                     event.event_type = 'modified'
